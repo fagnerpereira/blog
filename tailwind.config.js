4188c77b6052d40618dd2ed246dc20fdc460d3c4
Fagner Pereira <fagnerfpr@gmail.com>
2024-10-03 20:19:51 -0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "_layouts/**/*.html",
+    "_includes/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
